c1db7cd901ae72faccded49df07e969f807a7192
rohitashavaggarwal <rohitashav.aggarwal768@gmail.com>
2021-12-28 19:48:20 -0700

    Updating react states

--- 8-react-state/script.jsx
+++ 8-react-state/script.jsx
@@ -1,10 +1,23 @@
 class Clock extends React.Component {
   constructor(props) {
     super(props);
+    this.launchClock();
     this.state = {
       currentTime: new Date().toLocaleString(),
     };
   }
+
+  launchClock() {
+    setInterval(() => {
+      console.log("Updating time every 1 sec...");
+      // updating states
+      // setState() triggers render()
+      this.setState({
+        currentTime: new Date().toLocaleString(),
+      });
+    }, 1000);
+  }
+
   render() {
     return <div>{this.state.currentTime}</div>;
   }
